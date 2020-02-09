63b016a1e6a329651dcf402344e44af2ba2f035a
Thibault Coupin <thibault.coupin@gmail.com>
2020-02-09 21:35:49 +0100

    set app_env in travis build

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
-docker-compose run --rm tools composer install
-docker-compose run --rm tools yarn
-docker-compose run --rm tools yarn build
-docker-compose run --rm -v $PWD/:/local tools cp -r "vendor/" /local/
+docker-compose run -e APP_ENV=prod --rm tools composer install
+docker-compose run -e APP_ENV=prod --rm tools yarn
+docker-compose run -e APP_ENV=prod --rm tools yarn build
+docker-compose run -e APP_ENV=prod --rm -v $PWD/:/local tools cp -r "vendor/" /local/
