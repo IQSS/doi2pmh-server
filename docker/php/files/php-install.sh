126282ee1ffa36345488d94fac5387867a2329e0
plecor <146710476+plecor@users.noreply.github.com>
2024-11-06 09:31:56 +0100

    Merge minor changes - minor updates of dependencies - fix issues where the cache folder was created with the wrong user by `php bin/console cache:clear` in the Docker container

--- docker/php/files/php-install.sh
+++ docker/php/files/php-install.sh
@@ -6,3 +6,6 @@ curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin
 cd $SYMFONY_ROOT && composer install
 
 php bin/console assets:install
+
+mkdir -p /var/www/html/var/cache /var/www/html/var/log
+chown -R www-data:www-data /var/www/html/var
